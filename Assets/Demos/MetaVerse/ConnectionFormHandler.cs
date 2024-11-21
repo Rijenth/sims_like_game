5fa85b2cb081108e795897d6a373cc5fe4679d8c
Rijenth <a_rijenth@laposte.net>
2024-11-21 17:37:17 +0100

    init: login page

---
+++ Assets/Demos/MetaVerse/ConnectionFormHandler.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ConnectionFormHandler : MonoBehaviour
+{
+    private string _username;
+
+    public void setUsername(string username)
+    {
+        _username = username;
+    }
+
+    public void StartGame() 
+    {
+        if (string.IsNullOrEmpty(_username))
+        {
+            // Afficher le message d'erreur au niveau du form login
+            Debug.LogWarning("Username is empty. Please enter a valid username.");
+
+            return;
+        }
+        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}

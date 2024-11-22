c333a7adf2f3dc9ec78bcebefb38423f13d02414
benjamin <bandasavanh.b@akira-sarl.fr>
2024-11-22 11:01:25 +0100

    Refactorisation et amélioration de la gestion des bonus
    Le code a été modifié pour inclure les espaces de noms `Unity.VisualScripting` et `UnityEngine`. La classe `Bonus` a été reformattée pour corriger l'indentation et améliorer la lisibilité. La variable `Points` n'a plus de valeur par défaut. Une instance de `System.Random` a été ajoutée pour générer des valeurs aléatoires.
    
    La méthode `Start` a été simplifiée pour appeler directement `ActivateBonus`. La méthode `Update` et `ShouldHandleObject` ont été reformattées sans changement fonctionnel. La méthode `OnTriggerEnter` inclut maintenant une vérification de verrouillage (`LootLock`) avant de traiter le bonus, désactive le bonus au lieu de le détruire et planifie sa réactivation. Un message de débogage a été ajouté pour indiquer qu'un bonus a été collecté.
    
    Trois nouvelles méthodes privées ont été ajoutées : `ResetBonus` pour réactiver le bonus après une période aléatoire, `DesactivateBonus` pour désactiver le bonus et planifier sa réactivation, et `ActivateBonus` pour initialiser le bonus avec une couleur et des points aléatoires. Une classe interne `LootLock` a été ajoutée pour gérer l'état de verrouillage des bonus.

--- Assets/Demos/MetaVerse/Bonus.cs
+++ Assets/Demos/MetaVerse/Bonus.cs
@@ -1,34 +1,96 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class Bonus : MonoBehaviour
-{
+public class Bonus : MonoBehaviour {
     public LayerMask CollisionLayers;
-    public int Points = 1;
+    public int Points;
+    private System.Random random = new System.Random();
+
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    void Start() {
+        ActivateBonus();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
     }
 
+    private void ResetBonus() {
+        gameObject.SetActive(true);
+        Invoke("DesactivateBonus", random.Next(10, 15));
+    }
+
+    private void DesactivateBonus() {
+        if (gameObject.activeSelf) {
+            gameObject.SetActive(false);
+            Invoke("ResetBonus", random.Next(10, 15));
+        }
+    }
+
+
+    private void ActivateBonus() {
+
+        Transform cube = transform.Find("Cube");
+        if (cube != null) {
+            switch (Points = random.Next(1, 25)) {
+                case < 10:
+                    cube.GetComponent<Renderer>().material.color = new Color(0, 0, 255);
+                    break;
+                case < 20:
+                    cube.GetComponent<Renderer>().material.color = new Color(128, 0, 128);
+                    break;
+                case 25:
+                    cube.GetComponent<Renderer>().material.color = new Color(255, 215, 0);
+                    break;
+            }
+        }
+        gameObject.SetActive(true);
+        Invoke("DesactivateBonus", random.Next(10, 15));
+    }
+
+
     private bool ShouldHandleObject(Collider other) {
-       return (CollisionLayers.value & (1 << other.gameObject.layer)) > 0;
+        return (CollisionLayers.value & (1 << other.gameObject.layer)) > 0;
     }
 
     void OnTriggerEnter(Collider other) {
-      if (!ShouldHandleObject(other)) { return; }
+        if (!ShouldHandleObject(other)) { return; }
+        LootLock bonusLock = new LootLock();
 
-      CharacterScore cScore = other.gameObject.GetComponentInChildren<CharacterScore>();
-      if (cScore != null) {
-        cScore.AddScore(Points);
-      }
+        if (bonusLock.IsLocked) {
+            return;
+        }
+        bonusLock.Lock();
+        CharacterScore cScore = other.gameObject.GetComponentInChildren<CharacterScore>();
 
-      Destroy(gameObject);
+        if (cScore != null) {
+            cScore.AddScore(Points);
+        }
+        Debug.Log("Bonus Collected");
+        gameObject.SetActive(false);
+        Invoke("ResetBonus", random.Next(5, 15));
+    }
+
+
+    public class LootLock {
+
+        public bool IsLocked { get; private set; }
+
+        private readonly object _lock = new object();
+
+        public void Lock() {
+            lock (_lock) {
+                IsLocked = true;
+            }
+        }
+        public void Unlock() {
+            lock (_lock) {
+                IsLocked = false;
+            }
+        }
     }
 }
+

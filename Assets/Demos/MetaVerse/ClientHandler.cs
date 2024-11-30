b8f4d64b9f7541deb49f13a5336f005de9d4a485
Rijenth <a_rijenth@laposte.net>
2024-11-30 18:09:12 +0100

    wip: synchronized player position with animation

--- Assets/Demos/MetaVerse/ClientHandler.cs
+++ Assets/Demos/MetaVerse/ClientHandler.cs
@@ -8,7 +8,7 @@ using Player = Demos.MetaVerse.Player;
 public class ClientHandler : PlayerHandler
 {
     private IPEndPoint ServerEndpoint;
-    
+
     void Awake()
     {
         if (State.IsServer) gameObject.SetActive(false);
@@ -25,52 +25,20 @@ public class ClientHandler : PlayerHandler
         UDP.OnMessageReceived += (string message, IPEndPoint sender) =>
         {
             var decodedData = JsonUtility.FromJson<PlayerData>(message);
-            var username = decodedData.Username;
-            var position = decodedData.Position;
-            
-            var existingPlayer = Players.Find(player => player.GetComponent<Player>().Username == username);
 
-            if (!existingPlayer)
-            {
-                bool isMainCharacter = username == State.Username;
-
-                var newPlayer = AddPlayerAvatar(decodedData, isMainCharacter);
-                Players.Add(newPlayer);
-            }
-            else if (username != State.Username)
-            {
-                existingPlayer.transform.position = position;
-            }
+            SyncPlayerPosition(decodedData);
         };
     }
 
     // Update is called once per frame
     void Update()
     {
-        var avatar = Players.Find(player => player.GetComponent<Player>().Username == State.Username);
+        if (Time.time <= NextTimeout) return;
 
-        var username = State.Username;
-        var position = new Vector3(250, 0, 250);
-
-        if (avatar)
-        {
-            var player = avatar.GetComponent<Player>();
-
-            if (player)
-            {
-                username = player.Username;
-                position = player.Position;
-            }  
-        }
-        
-        PlayerData playerData = new PlayerData
-        {
-            Username = username,
-            Position = position,
-        };
-
-        string json = JsonUtility.ToJson(playerData);
+        var json = GeneratePlayerUDPData();
 
         UDP.SendUDPMessage(json, ServerEndpoint);
+
+        NextTimeout = Time.time + 0.5f;
     }
 }
